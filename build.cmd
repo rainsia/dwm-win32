a4d6a3330fa924b6c51e50fbea8d12f4831fe655
Yu Xia <rainsia@163.com>
2022-03-12 23:36:21 +0800

    modify configure and build script

--- build.cmd
+++ build.cmd
@@ -1,2 +1,4 @@
-cmake --build build -j %NUMBER_OF_PROCESSORS% || exit /b
+:: cmake --build build -j %NUMBER_OF_PROCESSORS% || exit /b
+cmake --build build\64bit --config Release
+
 :: cmake --build build || exit /b
--- configure.cmd
+++ configure.cmd
@@ -1,2 +1,4 @@
-cmake -S . -B build -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D USE_LUAJIT=ON || exit /b
+:: cmake -S . -B build -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D USE_LUAJIT=ON || exit /b
+cmake -G "Visual Studio 16 2019" -A x64 -S . -B "build\64bit" -DCMAKE_BUILD_TYPE=Release -D USE_LUAJIT=ON || exit /b
+
 copy build\compile_commands.json .\
